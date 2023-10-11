847a2ed984603ac5d817c30048389c5594a2ff6a
detwoled <simon-top@mail.ru>
2023-10-12 01:06:32 +0300

    table with rand colors and1 simular

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useEffect, useState } from "react";
 
-function App() {
+import './d.scss'
+
+const App: FC =() => {
+  const [cells, setCells] = useState<string[]>([])
+
+  const getRandNum = (value: number): number => Math.floor(Math.random() * value)
+  const getRandffset = (num: number, diff: number = 15): number => {
+    const max = Math.min(num + diff, 255)
+    const min = Math.max(num - diff, 0)
+    console.log({min, max})
+    const offest = getRandNum(max - min) + min
+    // const offest = max 
+    return offest 
+  }
+
+  const generateCells = (count: number): string[] => {
+    const diff = 10
+    const maxValue = 256
+
+    const colorArr = Array.from({length: 3}, () => getRandNum(maxValue))
+    const simularColorArr = Array.from(colorArr, i => getRandffset(i, diff))
+
+    const color = colorArr.toString() 
+    const simularColor = simularColorArr.toString() 
+
+    const cells = Array.from({length: count}, () => color)
+    cells[getRandNum(count)] = simularColor
+    console.log(colorArr,simularColorArr)
+    console.log(cells)
+    return cells
+    // const simularColorArr = Array.from({length: 3}, () => getRandNum(256))
+    // const simularColorArr
+    // setCells([color])
+    // console.log(color)
+
+    // const colors = []
+    // const delta = 30
+    // for i in range(loop):
+    //     const new_rgb = randint(max(0, x - delta), min(x + delta, 255))  
+    //     colors.push(new_rgb)                
+    // // return colors
+    
+  }
+
+  useEffect(() => {
+    // generateCells(81)
+    setCells(generateCells(25))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper">
+        <li className="cells-box">
+          {
+            cells.map((color, key) => (
+              <ul className="cell" 
+                key={key}
+                style={{backgroundColor: `rgb(${color})`}}
+              />
+            ))
+          }
+        </li>
+      </div>
     </div>
   );
 }
